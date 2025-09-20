b37f0016a0aa8b1287535497fd398d6d340770a6
Gaurav Rawat <85726780+GauravRawat7@users.noreply.github.com>
2025-09-20 08:45:56 +0530

    Create ui.py

---
+++ ui.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Freaky Classroom Assistant")
+
+question = st.text_input("puch be kya puchna hai:")
+# if question:
+#     answer = get_ai_answer(question)  # your Pathway + AI function
+#     st.write(answer)
+
+# st.subheader("Live updates:")
+# st.write(get_recent_updates())  # shows latest files/Excel changes
